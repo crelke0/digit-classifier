ee3622c84e0b19ca244b59dec2f22e07f23c0f4d
crelke0 <clarke.elke.mail@gmail.com>
2023-07-03 12:38:46 -0500

    updated to work with changes in network.rs

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod network;
 use crate::network::network::*;
 mod mnist_loader;
 use crate::mnist_loader::mnist_loader::get_mnist;
+mod activation_fns;
+use activation_fns::activation_fns::*;
 
 fn main() {
-  let data = get_mnist(20_000, 10_000);
+  let data = get_mnist(5_000, 10_000);
   let mut training_data = data.0;
   let test_data = data.1;
   println!("starting training...");
-  let mut network = Network::new(&[784, 30, 10], sigmoid, sigmoid_der);
+  let mut network = Network::new(&[784, 30, 10], &[sigmoid, sigmoid, sigmoid]);
   network.train(&mut training_data, &test_data, 10, 1000, 3.0, quadratic_cost_der, true);
 }
