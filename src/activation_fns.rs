98a984abc50ffac763872cb8e8019ab24b5c4776
crelke0 <clarke.elke.mail@gmail.com>
2023-07-05 13:48:00 -0500

    updated activation functions to work with changes in network.rs, and fixed the soft_max function

--- src/activation_fns.rs
+++ src/activation_fns.rs
@@ -2,38 +2,45 @@ pub mod activation_fns {
   use crate::network::network::Node;
 
   pub fn sigmoid(nodes: &mut [Node]) {
-    let act = |x: f32| {
+    let act = |x: f64| {
       if x > 20.0 {
         1.0
       } else if x < -20.0 {
         0.0
       } else {
-        1.0/(1.0 + f32::exp(-x))
+        1.0/(1.0 + f64::exp(-x))
       }
     };
-    for node in nodes {
-      let z = node.activation;
-      node.activation = act(z);
-      node.a_respect_z = act(z)*(1.0 - act(z));
+    for i in 0..nodes.len() {
+      let z = nodes[i].activation;
+      nodes[i].activation = act(z);
+      nodes[i].as_respect_z = (0..nodes.len())
+        .map(|j| if j == i { act(z)*(1.0 - act(z)) } else { 0.0 }).collect();
     }
   }
 
-  fn leaky_relu(nodes: &mut [Node]) {
+  pub fn leaky_relu(nodes: &mut [Node]) {
     let leak = 0.01;
-    for node in nodes {
-      let z = node.activation;
-      node.activation = if z > 0.0 { z } else { leak * z };
-      node.a_respect_z = if z > 0.0 { 1.0 } else { leak }
+    for i in 0..nodes.len() {
+      let z = nodes[i].activation;
+      nodes[i].activation = if z > 0.0 { z } else { leak * z };
+      nodes[i].as_respect_z = (0..nodes.len())
+        .map(|j| if j == i { if z > 0.0 { 1.0 } else { leak } } else { 0.0 }).collect();
     }
   }
 
   pub fn soft_max(nodes: &mut [Node]) {
-    let exps = nodes.iter().map(|node| f32::exp(node.activation)).collect::<Vec<f32>>();
-    let total = exps.iter().sum::<f32>();
+    let max = nodes.iter().map(|node| node.activation).fold(f64::NEG_INFINITY, |acc, e| acc.max(e));
+    let exps = nodes.iter().map(|node| f64::exp(node.activation - max)).collect::<Vec<f64>>();
+    let total = exps.iter().sum::<f64>();
     for i in 0..nodes.len() {
       nodes[i].activation = exps[i]/total;
-      let remaining = total - exps[i];
-      nodes[i].a_respect_z = remaining*exps[i]/((exps[i] + remaining).powf(2.0));
     }
+    let zs = nodes.iter().map(|node| node.activation).collect::<Vec<f64>>();
+    for i in 0..nodes.len() {
+      nodes[i].as_respect_z = (0..nodes.len())
+        .map(|j| if j == i { nodes[i].activation*(1.0 - nodes[i].activation) } else { -nodes[i].activation*nodes[j].activation }).collect();
+    }
+    
   }
 }
