820bc37f996807d487e3ff1bd5862ce9b8c1a0c8
crelke0 <clarke.elke.mail@gmail.com>
2023-07-05 13:48:27 -0500

    now using soft max and cross entropy

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ mod activation_fns;
 use activation_fns::activation_fns::*;
 
 fn main() {
-  let data = get_mnist(5_000, 10_000);
+  let data = get_mnist(50_000, 10_000);
   let mut training_data = data.0;
   let test_data = data.1;
   println!("starting training...");
-  let mut network = Network::new(&[784, 30, 10], &[sigmoid, sigmoid, sigmoid]);
-  network.train(&mut training_data, &test_data, 10, 1000, 3.0, quadratic_cost_der, true);
+  let mut network = Network::new(&[784, 30, 10], &[sigmoid, sigmoid, soft_max]);
+  network.train(&mut training_data, &test_data, 10, 1000, 0.5, crossentropy_cost_der, true);
 }
