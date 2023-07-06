6b7086aa45c67a66f6ccc85777c3a16c7d3d77eb
crelke0 <clarke.elke.mail@gmail.com>
2023-07-06 13:34:08 -0500

    main updated to work with changes in network

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ mod functions;
 use functions::functions::*;
 
 fn main() {
-  let data = get_mnist(1_000, 10_000);
+  let data = get_mnist(10_000, 10_000);
   let mut training_data = data.0;
   let test_data = data.1;
   println!("starting training...");
-  let mut network = Network::new(&[784, 100, 20, 10], &[ActivationFn::Sigmoid, ActivationFn::Sigmoid, ActivationFn::Sigmoid, ActivationFn::SoftMax]);
+  let mut network = Network::new(&[784, 100, 20, 10], &[ActivationFn::Sigmoid, ActivationFn::Sigmoid, ActivationFn::SoftMax]);
   network.train(&mut training_data, &test_data, 10, 100, 0.3, 0.002, Cost::CrossEntropy, true);
 }
